50504260e2ad370d384fa029c7e601e4ddd12abe
BrokenTrident <chrisgill313@gmail.com>
2025-06-04 10:39:28 -0400

    added form

--- components/card_demo.tsx
+++ components/card_demo.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { z } from "zod"
+
 
 import {
   Card,
